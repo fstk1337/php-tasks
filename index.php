428341b0f7bb2e44b83758b0e8b938ff61a49056
fstk1337 <vit.shvaichuk@gmail.com>
2021-04-05 20:28:19 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>10 Tasks</title>
+</head>
+<body>
+    <h1>My 10 tasks:</h1>
+    <ul>
+        <li>
+            <a href="task_1.php">Task 1</a>
+        </li>
+        <li>
+            <a href="task_2.php">Task 2</a>
+        </li>
+        <li>
+            <a href="task_3.php">Task 3</a>
+        </li>
+        <li>
+            <a href="task_4.php">Task 4</a>
+        </li>
+        <li>
+            <a href="task_5.php">Task 5</a>
+        </li>
+        <li>
+            <a href="task_6.php">Task 6</a>
+        </li>
+        <li>
+            <a href="task_7.php">Task 7</a>
+        </li>
+        <li>
+            <a href="task_8.php">Task 8</a>
+        </li>
+        <li>
+            <a href="task_9.php">Task 9</a>
+        </li>
+        <li>
+            <a href="task_10.php">Task 10</a>
+        </li>
+    </ul>
+</body>
+</html>
